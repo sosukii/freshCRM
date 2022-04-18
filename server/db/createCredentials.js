57d1bd71bd8594cf5d95146c06e85da0aa9dacde
Svetlana Trukhina <sweta4ca@mail.ru>
2022-04-18 09:39:44 +0700

    fix: remove dbName from connection string

--- server/db/createCredentials.js
+++ server/db/createCredentials.js
@@ -1,5 +1,5 @@
-import { question } from 'readline-sync';
-import { existsSync, unlinkSync, writeFileSync } from 'fs';
+const { question } = require('readline-sync');
+const { existsSync, unlinkSync, writeFileSync } = require('fs');
 
 if(existsSync('./credentials.json')) {
     console.log('credentials.json already exist. the old file be deleted.')
@@ -8,18 +8,17 @@ if(existsSync('./credentials.json')) {
     })
 }
 
-const connectionToMongoString = question(`Enter you're mongo connection string (for example: mongodb+srv://Alexey555:myAwesomePassword777@cluster0.zaaoe.mongodb.net): `)
+const connectionToMongoString = question(`Enter you're mongo connection string with db name (for example: mongodb+srv://Alexey555:myAwesomePassword777@cluster0.zaaoe.mongodb.net/myprettydb): `)
 
 if(connectionToMongoString) {
     const authData = {
-        "connectionString":connectionToMongoString,
-        "dbNAME": "myprettydb"
+        "connectionString":connectionToMongoString
     }
 
     writeFileSync('./credentials.json', JSON.stringify(authData), err => {
         console.log('creating credentials.json error: ', err)
     })
-    console.log('credentials.json successfully created!')
+    console.log('♡ credentials.json successfully created!')
 } else{
     console.log(`Wrong connection string. Please try again.`);
 }
